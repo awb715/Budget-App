f1c83e44769ef1dfb58a00694d0b367f89408f09
Aaron Bell <aaronbellwebdev@gmail.com>
2017-07-03 16:01:50 -0700

    Using AXIOS, succesfully return Tax amounts

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const tax = require('./scripts/taxee.js')
+const axios = require('axios');
 var app = express();
-
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+//render index.html
 app.use(express.static(__dirname + '/views'));
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index.html');
 });
 
+
+app.post('/info', function (req, res) {
+    var State = req.body.State;
+    var income = Number(req.body.income);
+    axios.post('https://taxee.io/api/v2/calculate/2017', {
+        //data
+        "exemptions": 1
+        , "filing_status": "single"
+        , "pay_periods": 1
+        , "pay_rate": income || 100000
+        , "state": State || "NY"
+    }, {
+        headers: {
+            'Authorization': "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUElfS0VZX01BTkFHRVIiLCJodHRwOi8vdGF4ZWUuaW8vdXNlcl9pZCI6IjU4NDQ4MTA4Mzg2NjhhMTU4ZDU0ZmIzNSIsImh0dHA6Ly90YXhlZS5pby9zY29wZXMiOlsiYXBpIl0sImlhdCI6MTQ5OTA1MzU0NX0.pOwC5JEC7trLaaZVgHHGu_rvN0-EGa3RMm8BgJ-M9gk"
+    
+                //headers
+        }
+    }).then(function (response) {
+        console.log(response.data, 'this is RESPONSE')
+        res.send(response.data.annual);
+    }).catch(function (error) {
+        console.log(error);
+    });
+});
 app.listen(3000);
