acff7704962cea82722b68ecce78ba07509b855c
Aaron Bell <aaronbellwebdev@gmail.com>
2017-07-20 23:26:25 -0400

    Port

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.post('/Info', function (req, res) {
     app.get('/Tool' , function(req, res){
         res.render('send');
     });
-app.listen(3000, function(){
+app.listen(process.env.PORT||3000, function(){
     
     console.log('Server Running on Port 3000')
 });
