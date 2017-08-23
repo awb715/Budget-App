e691d6bd7f61c0f3bdc5ab7d8199dcfccd2160d3
Aaron Bell <aaronbellwebdev@gmail.com>
2017-08-22 22:16:34 -0400

    Styling and beginning of progress bar

--- views/js/app.js
+++ views/js/app.js
@@ -1,3 +1,55 @@
+var usStates = {
+    Alabama: "AL"
+    , Alaska: "AK"
+    , Arizona: "AZ"
+    , Arkansas: "AR"
+    , California: "CA"
+    , Colorado: "CO"
+    , Connecticut: "CT"
+    , Delaware: "DE"
+    , Florida: "FL"
+    , Georgia: "GA"
+    , Hawaii: "HI"
+    , Idaho: "ID"
+    , Illinois: "IL"
+    , Indiana: "IN"
+    , Iowa: "IA"
+    , Kansas: "KS"
+    , Kentucky: "KY"
+    , Louisiana: "LA"
+    , Maine: "ME"
+    , Maryland: "MD"
+    , Massachusetts: "MA"
+    , Michigan: "MI"
+    , Minnesota: "MN"
+    , Mississippi: "MS"
+    , Missouri: "MO"
+    , Montana: "MT"
+    , Nebraska: "NE"
+    , Nevada: "NV"
+    , New_Hampshire: "NH"
+    , New_Jersey: "NJ"
+    , New_Mexico: "NM"
+    , New_York: "NY"
+    , North_Carolina: "NC"
+    , North_Dakota: "ND"
+    , Ohio: "OH"
+    , Oklahoma: "OK"
+    , Oregon: "OR"
+    , Pennsylvania: "PA"
+    , Rhode_Island: "RI"
+    , South_Carolina: "SC"
+    , South_Dakota: "SD"
+    , Tennessee: "TN"
+    , Texas: "TX"
+    , Utah: "UT"
+    , Vermont: "VT"
+    , Virginia: "VA"
+    , Washington: "WA"
+    , West_Virginia: "WV"
+    , Wisconsin: "WI"
+    , Wyoming: "WY"
+};
 //will load the proper questions on the page
 //https://github.com/kswedberg/jquery-smooth-scroll
 var quest = ['../partials/income.hbs', '../partials/state.hbs', '../partials/zip.hbs', '../partials/roomate.hbs'];
@@ -7,26 +59,31 @@ function Obj(name, val) {
     event.preventDefault();
     $('.form').load(quest[iterator]).addClass('animated fadeIn');
     if (name) {
+        if (name === "state") {
+            val = usStates[stateConvert(val)];
+        }
         data[name] = val;
         console.log(data);
     }
     iterator++;
 }
 var data = {};
-
-
- //page scroll sliding
-    $(function () {
-        $('a[href*="#"]:not([href="#"])').click(function () {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                if (target.length) {
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000);
-                    return false;
-                }
+//page scroll sliding
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
             }
-        });
+        }
     });
+});
+
+function stateConvert(value) {
+    return value.split(" ").join('_').toString();
+}
