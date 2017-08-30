404ed538633c678ee351591cf8b9ca938199845b
Aaron Bell <aaronbellwebdev@gmail.com>
2017-08-29 20:48:17 -0400

    Clean app.js and uneligible clicks on bar

--- views/js/app.js
+++ views/js/app.js
@@ -1,80 +1,12 @@
-var stateAbv = {
-        "AL": "Alabama"
-        , "AK": "Alaska"
-        , "AS": "American Samoa"
-        , "AZ": "Arizona"
-        , "AR": "Arkansas"
-        , "CA": "California"
-        , "CO": "Colorado"
-        , "CT": "Connecticut"
-        , "DE": "Delaware"
-        , "DC": "District Of Columbia"
-        , "FM": "Federated States Of Micronesia"
-        , "FL": "Florida"
-        , "GA": "Georgia"
-        , "GU": "Guam"
-        , "HI": "Hawaii"
-        , "ID": "Idaho"
-        , "IL": "Illinois"
-        , "IN": "Indiana"
-        , "IA": "Iowa"
-        , "KS": "Kansas"
-        , "KY": "Kentucky"
-        , "LA": "Louisiana"
-        , "ME": "Maine"
-        , "MH": "Marshall Islands"
-        , "MD": "Maryland"
-        , "MA": "Massachusetts"
-        , "MI": "Michigan"
-        , "MN": "Minnesota"
-        , "MS": "Mississippi"
-        , "MO": "Missouri"
-        , "MT": "Montana"
-        , "NE": "Nebraska"
-        , "NV": "Nevada"
-        , "NH": "New Hampshire"
-        , "NJ": "New Jersey"
-        , "NM": "New Mexico"
-        , "NY": "New York"
-        , "NC": "North Carolina"
-        , "ND": "North Dakota"
-        , "MP": "Northern Mariana Islands"
-        , "OH": "Ohio"
-        , "OK": "Oklahoma"
-        , "OR": "Oregon"
-        , "PW": "Palau"
-        , "PA": "Pennsylvania"
-        , "PR": "Puerto Rico"
-        , "RI": "Rhode Island"
-        , "SC": "South Carolina"
-        , "SD": "South Dakota"
-        , "TN": "Tennessee"
-        , "TX": "Texas"
-        , "UT": "Utah"
-        , "VT": "Vermont"
-        , "VI": "Virgin Islands"
-        , "VA": "Virginia"
-        , "WA": "Washington"
-        , "WV": "West Virginia"
-        , "WI": "Wisconsin"
-        , "WY": "Wyoming"
-    }
-    //will load the proper questions on the page
-    //https://github.com/kswedberg/jquery-smooth-scroll
-var quest = ['../partials/income.hbs', '../partials/state.hbs', '../partials/rent.hbs', '../partials/zip.hbs', '../partials/roomate.hbs', "../partials/summary.hbs"];
-var iterator = 0;
+var quest = ['../partials/income.hbs', '../partials/state.hbs', '../partials/rent.hbs', '../partials/zip.hbs', '../partials/roomate.hbs', "../partials/summary.hbs"]; //various templates and questions
+var iterator = 0; //used to access next question on submit of answers
 //creates object that gets sent to api
 function Obj(name, vale) {
-    event.preventDefault(); //prevents pg refresh
-    $('.form').load(quest[iterator], function () {
-        $('.bar p#' + name).addClass('enter').next().css({
-            "border": "2px solid black"
-            , "pointer-events": "auto"
-            , "cursor": "pointer"
-        });
-        $('#p').val(data[name]);
-    });
-    //loads next html question to page
+    if (iterator == 0) {
+        $('p#income').addClass('next');
+    }
+    event.preventDefault();
+    next(name, vale);
     if (name) {
         data[name] = vale;
     }
@@ -85,33 +17,25 @@ function Obj(name, vale) {
 var data = {};
 //user can click already submitted values to load that orignal question and change it/
 function redo(q, id) {
-    q = Number(q);
-    //if the current queston on the page is not the one being clicked we then change it to the one being clicked
-    if (q + 1 != iterator) {
+    //only call if this element as a certain class 
+    if ($('p#' + id).hasClass('next') && q + 1 != iterator) {
         $('.form').load(quest[q], function () {
-            $('#p').val(data[id]);
+            setter(id);
+            iterator = q + 1;
         });
-        iterator = q + 1;
-    };
+    }
 };
 $(document).on('click', '.rentanswer p', function () {
     var answer = $(this).text();
     if (answer == "Yes") {
-        $('.form').load('../partials/rentinput.hbs', function () {
-            $('#p').val(data.rent);
-        });
+        $('.form').load('../partials/rentinput.hbs');
         quest[2] = "../partials/rentinput.hbs";
-        console.log(quest[2]);
     }
     else {
         $('.form').load(quest[iterator], function () {
-            $('.bar p#rent').css("border", "2px solid black").addClass('enter').next().css({
-                "border": "2px solid black"
-                , "pointer-events": "auto"
-                , "cursor": "pointer"
-            });
+            styler('rent');
+            setter('zip');
             iterator++;
-            $('#p').val(data[zip]);
         });
     };
 });
@@ -130,3 +54,26 @@ $(function () {
         };
     });
 });
+
+function next(name, vale) { //will load next question
+    $('.form').load(quest[iterator], function () {
+        styler(name);
+        setter(nextAppend[name]);
+    });
+}
+var nextAppend = { //allows for past answer to be set as proper value on next question when resubmitting with Obj function
+    income: 'state'
+    , state: 'rent'
+    , rent: 'zip'
+    , zip: 'roommate'
+};
+
+function setter(prop) {
+    if (data[prop]) { //sets value of input being loaded
+        $('#p').val(data[prop]);
+    }
+};
+
+function styler(id) {
+    $('.bar p#' + id).addClass('enter').next().addClass('next');
+}
